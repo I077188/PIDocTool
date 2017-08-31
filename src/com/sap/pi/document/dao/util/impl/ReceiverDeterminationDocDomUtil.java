a1bb6acfd58c4520e69d02bf177bbfe0a2413082
Kelly Zheng <kelly.zheng@sap.com>
2017-08-31 14:07:58 +0800

    Generate receiver destination goup

--- src/com/sap/pi/document/dao/util/impl/ReceiverDeterminationDocDomUtil.java
+++ src/com/sap/pi/document/dao/util/impl/ReceiverDeterminationDocDomUtil.java
@@ -1,5 +1,11 @@
 package com.sap.pi.document.dao.util.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sap.pi.document.util.dao.CONSTAINTS;
+import com.sap.pi.document.util.dao.Item;
+import com.sap.pi.document.util.impl.DocDomGroupUtilImpl;
 import com.sap.xi.basis.IntegratedConfiguration;
 
 public class ReceiverDeterminationDocDomUtil {
@@ -11,11 +17,19 @@ public class ReceiverDeterminationDocDomUtil {
 		rrDocDomUtil.generateRRDomFile(integratedConfiguration);
 
 		// generate external Receiver Rule Part
+		ERRDocDomUtil errDocDomUtil = new ERRDocDomUtil();
+		errDocDomUtil.generateERRDomFile(integratedConfiguration);
 
 
 		// generate dynamic Receiver Rule Part
 		DRRDocDomUtil drrDocDomUtil = new DRRDocDomUtil();
 		drrDocDomUtil.generateDRRDomFile(integratedConfiguration);
+
+		// dom group Receiver Destination, write back to type ICO
+		DocDomGroupUtilImpl domGroupUtil = new DocDomGroupUtilImpl();
+		List<Item> rDestinationDomGroupItems = new ArrayList<>();
+		rDestinationDomGroupItems.add(new Item("$Main_Name", "receiverDestination"));
+		domGroupUtil.generateDomGroupFile(CONSTAINTS.DOMGROUP_RD, rDestinationDomGroupItems, "ICO", true);
 	}
 
 }
