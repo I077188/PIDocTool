638fdbf89ff42226ce4b20351fd6a77a79da4708
Kelly Zheng <kelly.zheng@sap.com>
2017-09-01 12:03:55 +0800

    Modify Receiver Determination and RD template

--- src/com/sap/pi/document/dao/util/impl/ReceiverDeterminationDocDomUtil.java
+++ src/com/sap/pi/document/dao/util/impl/ReceiverDeterminationDocDomUtil.java
@@ -6,11 +6,15 @@ import java.util.List;
 import com.sap.pi.document.util.dao.CONSTAINTS;
 import com.sap.pi.document.util.dao.Item;
 import com.sap.pi.document.util.impl.DocDomGroupUtilImpl;
+import com.sap.pi.document.util.impl.OtherUtil;
 import com.sap.xi.basis.IntegratedConfiguration;
+import com.sap.xi.basis.Receivers;
 
 public class ReceiverDeterminationDocDomUtil {
 
 	public void generateRDDomGroupFile(IntegratedConfiguration integratedConfiguration) {
+
+		DocDomGroupUtilImpl domGroupUtil = new DocDomGroupUtilImpl();
 
 		// generate Receiver Rule Part
 		ReceiverRuleDocDomUtil rrDocDomUtil = new ReceiverRuleDocDomUtil();
@@ -26,10 +30,33 @@ public class ReceiverDeterminationDocDomUtil {
 		drrDocDomUtil.generateDRRDomFile(integratedConfiguration);
 
 		// dom group Receiver Destination, write back to type ICO
-		DocDomGroupUtilImpl domGroupUtil = new DocDomGroupUtilImpl();
-		List<Item> rDestinationDomGroupItems = new ArrayList<>();
-		rDestinationDomGroupItems.add(new Item("$Main_Name", "receiverDestination"));
-		domGroupUtil.generateDomGroupFile(CONSTAINTS.DOMGROUP_RD, rDestinationDomGroupItems, "ICO", false);
+
+		Receivers receiver = integratedConfiguration.getReceivers();
+		if (receiver != null) {
+			String noReceiverBehavior = OtherUtil
+					.getValue(receiver.getNoReceiverBehaviour().toString());
+			String partyID = OtherUtil.getValue(receiver.getNoReceiverReceiver().getPartyID());
+			String componentID = OtherUtil.getValue(receiver.getNoReceiverReceiver().getComponentID());
+			String noReceiverReceiver = "N/A";
+
+			if (partyID != "N/A") {
+				noReceiverReceiver = partyID + "|";
+				if (componentID != "N/A") {
+					noReceiverReceiver = noReceiverReceiver + componentID;
+				}
+			} else {
+				if (componentID != "N/A") {
+					noReceiverReceiver = componentID;
+				}
+			}
+
+			List<Item> rDestinationDomGroupItems = new ArrayList<>();
+			rDestinationDomGroupItems.add(new Item("$Main_Name", "receiverDestination"));
+			rDestinationDomGroupItems.add(new Item("$NoReceiverBehavior_Value", noReceiverBehavior));
+			rDestinationDomGroupItems.add(new Item("$NoReceiverReceiver_Value", noReceiverReceiver));
+			domGroupUtil.generateDomGroupFile(CONSTAINTS.DOMGROUP_RD, rDestinationDomGroupItems, "ICO", false);
+		}
+
 	}
 
 }
